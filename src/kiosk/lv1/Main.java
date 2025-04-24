e860a212db36ebcacb7fb0a73e63f5be5f3dc073
zzzzdong <jieun8496@icloud.com>
2025-04-24 15:35:42 +0900

    키오스크 Lv1

---
+++ src/kiosk/lv1/Main.java
@@ -0,0 +1,36 @@
+package kiosk.lv1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료");
+            System.out.println("숫자를 입력해주세요: ");
+            int num = sc.nextInt();
+            if (num == 1) {
+                System.out.println("ShackBurger | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            } else if(num == 2) {
+                System.out.println("SmokeShack | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            } else if(num == 3) {
+                System.out.println("Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            } else if(num == 4) {
+                System.out.println("Hamburger | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            } else if(num == 0) {
+                System.out.println("종료되었습니다.");
+                break;
+            } else {
+                System.out.println("다시 입력해주세요.");
+                continue;
+            }
+            System.out.println("선택된 메뉴 = " + num);
+        }
+    }
+}
