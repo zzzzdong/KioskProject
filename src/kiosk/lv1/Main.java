87ae07eb0d3088384c03345d805a0ba55025d67d
zzzzdong <jieun8496@icloud.com>
2025-05-01 01:23:48 +0900

    키오스크 Lv2

--- src/kiosk/lv1/Main.java
+++ src/kiosk/lv1/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("3. Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
             System.out.println("4. Hamburger | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             System.out.println("0. 종료");
-            System.out.println("숫자를 입력해주세요: ");
+            System.out.println("메뉴 번호를 입력해주세요: ");
             int num = sc.nextInt();
             if (num == 1) {
                 System.out.println("ShackBurger | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
